0b4009b8d1c6fd715c8f7e91fd1e3cef2b16fd33
fjribgs <fajribagaskara@upi.edu>
2025-11-21 15:39:03 +0700

    feat: added new concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,86 @@
 import { useState } from "react";
-import scenes from "./scenes/scenes";
 
 function App() {
-  const [currentScene, setCurrentScene] = useState("start");
-  const scene = scenes[currentScene];
+  const [isAVisited, setIsAVisited] = useState(false);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
+  const [currentPage, setCurrentPage] = useState('selection');
 
-      <h1>Choice Game</h1>
+  const handleVisitedOptionA = () => {
+    setIsAVisited(true);
 
-      <p>{scene.text}</p>
+    setCurrentPage('selection');
+    console.log('Opsi A Selesai. Fitur B terbuka!');
+  };
 
+  const handleGoBack = () => {
+    setCurrentPage('selection');
+  };
+
+  if (currentPage === 'selection') {
+    return (
       <div>
 
-        {scene.choices.map((choice, idx) => (
-          <button
-            key={idx}
-            onClick={() => setCurrentScene(choice.next)}>
-              {choice.text}
-            </button>
-        ))}
+          <h2>Pilih Opsi: </h2>
+
+          <button onClick={() => setCurrentPage('optionA')}
+            className="cursor-pointer">
+            Jelajahi A
+          </button>
+
+          <button onClick={() => setCurrentPage('optionB')}
+            className="cursor-pointer">
+            Jelajahi B
+          </button>
+
+          <p>Status Kunci B: {isAVisited ? 'TERBUKA' : 'TERKUNCI'}</p>
 
       </div>
+    )
+  }
 
-    </div>
+  if (currentPage === 'optionA') {
+    return (
+      <div>
+
+        <h3>Konten Opsi A</h3>
+        <p>Yaya</p>
+        <button onClick={handleVisitedOptionA}>
+          Kembali
+        </button>
+
+      </div>
+    )
+  }
+
+  if (currentPage === 'optionB') {
+    return (
+      <div>
+
+        <h3>Konten Opsi B</h3>
+        <p>Jing</p>
+
+        <button onClick={() => setCurrentPage('optionB_advanced')}
+          className={`${isAVisited ? 'flex' : 'hidden'}`}>Lanjut Ke Halaman B</button>
+        <button onClick={handleGoBack}>Kembali ke A</button>
+
+      </div>
+    )
+  }
+
+  if (currentPage === 'optionB_advanced') {
+    return (
+      <div style={{ padding: '20px', border: '1px solid #FF9800', borderRadius: '8px' }}>
+        <h3>🏆 Konten Opsi B Lanjutan!</h3>
+        <p>Anda berhasil mencapai halaman ini karena telah menyelesaikan prasyarat Opsi A.</p>
+        <button onClick={handleGoBack}>
+          Kembali ke Pilihan
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>Nigga</div>
   )
 }
 
