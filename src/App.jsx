a2c0e9cc6a342d6b6fed3878d67c4677541f156d
fjribgs <fajribagaskara@upi.edu>
2025-11-22 22:50:56 +0700

    feat: added new page

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,67 @@ function App() {
         )
     }
 
+    if (currentPage === 'nothing') {
+        return (
+
+            <div id="nothing"
+                className="flex flex-col text-center gap-6 w-screen px-[18px] lg:px-[60px] h-screen py-10">
+
+                <div className="flex-1 text-start text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] border-2 border-(--normal) rounded-2xl px-5 py-[26px] h-full font-light">
+
+                    <p>
+                        Well, while the rain is quite an entertaining sight itself, it will probably become boring within the next hour.
+                    </p>
+
+                </div>
+
+                <div className="flex flex-col flex-1 w-full h-full gap-2.5">
+
+                    <button onClick={() => setCurrentPage('bedroom')}
+                        className="text-(--bg) text-[18px] xl:text-[22px] bg-(--normal) rounded-2xl px-20 py-2.5 font-semibold hover:bg-(--hover) hover:text-[20px] hover:xl:text-[26px] active:bg-(--active) active:xl:text-[20px] transition-all duration-200 cursor-pointer w-full">
+                        Go Back.
+                    </button>
+
+                </div>
+
+            </div>
+
+        )
+    }
+
+    if (currentPage === 'readBook') {
+        return (
+
+            <div id="readBook"
+                className="flex flex-col text-center gap-6 w-screen px-[18px] lg:px-[60px] h-screen py-10">
+
+                <div className="flex-1 text-start text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] border-2 border-(--normal) rounded-2xl px-5 py-[26px] h-full font-light">
+
+                    <p>
+                        (What book will you read?)
+                    </p>
+
+                </div>
+
+                <div className="flex flex-col flex-1 w-full h-full gap-2.5">
+
+                    <button onClick={() => setCurrentPage('continuePrideNPrejudice')}
+                        className="text-(--bg) text-[18px] xl:text-[22px] bg-(--normal) rounded-2xl px-20 py-2.5 font-semibold hover:bg-(--hover) hover:text-[20px] hover:xl:text-[26px] active:bg-(--active) active:xl:text-[20px] transition-all duration-200 cursor-pointer w-full">
+                        Continue Pride and Prejudice (currently on chapter 44)
+                    </button>
+
+                    <button onClick={() => setCurrentPage('startLePetitPrince')}
+                        className="text-(--bg) text-[18px] xl:text-[22px] bg-(--normal) rounded-2xl px-20 py-2.5 font-semibold hover:bg-(--hover) hover:text-[20px] hover:xl:text-[26px] active:bg-(--active) active:xl:text-[20px] transition-all duration-200 cursor-pointer w-full">
+                        Start Le Petit Prince!
+                    </button>
+
+                </div>
+
+            </div>
+
+        )
+    }
+
 }
 
 export default App;
