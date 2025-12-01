728a3a27651af0ddf326809f93fcc844577c827e
fjribgs <fajribagaskara@upi.edu>
2025-12-01 23:36:31 +0700

    added new branch gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/choice-game/",
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: "/choice-game/",
 })
